252569eaecb2abeccbb22a8149d2922be7beefe4
Jeremy <jeremyvanderdoes@gmail.com>
2022-11-04 17:00:33 -0400

    Add data and some dependencies

---
+++ R/diabetes.R
@@ -0,0 +1,21 @@
+#' Diabetes Data
+#'
+#' A subset of mass cytometry data from diabetes patients.
+#'
+#' Upcoming: Get more complete data set
+#'
+#' @format ## `diabetes`
+#' A data frame with 253,777 rows and 6 columns:
+#' \describe{
+#'   \item{Person}{ID for each person}
+#'   \item{Image}{ID for each image}
+#'   \item{x,y}{The cooridinates of each cell}
+#'   \item{cellType}{Classified cell type}
+#'   \item{Stage}{Stage of the patient}
+#'   ...
+#' }
+#' @source Currently, <https://www.bioconductor.org/packages/release/bioc/html/spicyR.html>
+#'
+#' Consider use of <https://data.mendeley.com/datasets/cydmwsfztj> or
+#' <https://rdrr.io/github/BodenmillerGroup/imcdatasets/man/Damond_2019_Pancreas.html>.
+"diabetes"
--- data-raw/diabetes.R
+++ data-raw/diabetes.R
@@ -1,24 +1,16 @@
+## code to prepare `diabetes` dataset goes here
+library(spicyR)
 
-#' Get Diabetes Data
-#'
-#' This (internal) function was used to generate Diabetes.RData
-#'
-#' @return data.frame with diabetes data
-#' @export
-#'
-#' @examples
-#' diabetes <- getDiabetesData()
-#' usethis::use_data(diabetes)
 getDiabetesData <- function(){
   # This function get the diabetes data and organizes it from spicyR
-  
+
   #####
   # Get Data
   data("diabetesData")
-  
+
   tmp <- as.data.frame(cellSummary(diabetesData))
   tmp1 <- as.data.frame(imagePheno(diabetesData))
-  
+
   #####
   # Organize Data
   data <- data.frame('Person'=rep(NA,length(tmp$imageID)),
@@ -26,18 +18,21 @@ getDiabetesData <- function(){
                      'x'=NA, 'y'=NA,
                      'cellType'=NA,
                      'Stage'=NA)
-  
+
   data$Image <- tmp$imageID
   data$x <- tmp$x
   data$y <- tmp$y
   data$cellType <- as.character(tmp$cellType)
-  
+
   for(i in 1:length(data$Person)){
     data$Person[i] <-
       tmp1[as.character(tmp1$imageID)==as.character(data$Image[i]), 'case']
-    data$Stage[i] <- 
+    data$Stage[i] <-
       as.character(tmp1[as.character(tmp1$imageID)==as.character(data$Image[i]), 'stage'])
   }
-  
+
   data
 }
+diabetes <- getDiabetesData()
+
+usethis::use_data(diabetes, overwrite = TRUE)
