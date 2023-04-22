58f117bef8ea7e6785dd6a4cf0ce727188027079
Jeremy <jeremyvanderdoes@gmail.com>
2023-04-22 17:16:51 -0400

    Update tmpR simulation files

--- tmpR/change_simulations_small.R
+++ tmpR/change_simulations_small.R
@@ -1,6 +1,12 @@
 nSims <- 100
-changes <- c(1/25,1/50,1/100)
+changes <- c(1/10, 1/15, 1/20, 1/40, 1/80, 1/160)
+baseline <- changes[1]
 results <- list()
+
+cells <- paste0('c',1:4)
+cells_interactions <- rbind(data.frame(t(combn(cells,2))),
+                            data.frame('X1'=cells,'X2'=cells))
+
 
 for(c in 1:length(changes)){
   cat(paste0('\n- Change: ',changes[c],'\n-- (',nSims,'): '))
@@ -8,11 +14,11 @@ for(c in 1:length(changes)){
   VarVI <- VarLVI <-
     VarVI_r <- VarLVI_r <-
     VarVI_o <- VarLVI_o <-
+    VarVI_max <- VarVI_o_max <-
+    VarLVI_max <- VarLVI_o_max <-
     data.frame('var'=c('age',
-                       as.vector(sapply(paste0('c',1:4,'_'),FUN = function(x){paste0(x,'c',1:4)})),
+                       paste(cells_interactions$X1,cells_interactions$X2,sep='_'),
                        'gender'))
-  RedVI1 <- RedLVI1 <- OrangeVI1 <- OrangeLVI1 <-
-    RedVI2 <- RedLVI2 <- OrangeVI2 <- OrangeLVI2 <- rep(NA,nSims)
 
   set.seed(c*12345)
 
@@ -22,8 +28,9 @@ for(c in 1:length(changes)){
     dat <- simulatePP(cellVarData=
                         data.frame('stage'=c(0,1),
                                    'c1'=c(0,0),
-                                   'c2'=c(1/25,changes[c]), 'c3'=c(1/50,1/50),
-                                   'c4'=c(0,0)),
+                                   'c2'=c(baseline,changes[c]),
+                                   'c3'=c(1/50,1/50),
+                                   'c4'=c(1/10,1/10)),
                       cellKappaData=data.frame(
                         'cell'=paste0('c',1:4),
                         'clusterCell'=c(NA,'c1','c1', NA),
@@ -35,7 +42,8 @@ for(c in 1:length(changes)){
                       silent=T)
     pcaData <- getPCAData(data = dat,repeatedUniqueId='Image',
                           xRange = c(0,1), yRange = c(0,1),
-                          silent=T)
+                          agents_df = cells_interactions,
+                          silent=TRUE)
     pcaMeta <- simulateMeta(pcaData,
                             metaInfo = data.frame(
                               'var'=c('gender','age'),
@@ -43,30 +51,16 @@ for(c in 1:length(changes)){
                               'Stage_0'=c('0.5','25'),
                               'Stage_1'=c('0.5','25')))
     # Fit RF
-    rfcv <- funkyRandomForest(data=pcaMeta,
-                              outcome = 'Stage',
-                              unit = 'Person',
-                              metaNames=c('gender','age'),
-                              silent = T)
+    rfcv <- funkyModel(data=pcaMeta,
+                       outcome = 'Stage', unit = 'Person',
+                       metaNames=c('gender','age'), silent = TRUE)
 
     # Org Data
     tmp <- rfcv$VariableImportance[,c('var','est','sd')]
     tmp <- tmp[order(-tmp$est),]
     tmp$lower1sd <- tmp$est-tmp$sd
 
-    # Orange/Red for one c1_c2
-    idx <- which(tmp$var=='c1_c2')
-    RedVI1[i] <- tmp[idx,'est']> rfcv$NoiseCutoff
-    RedLVI1[i] <- tmp[idx,'lower1sd']> rfcv$NoiseCutoff
-    OrangeVI1[i] <- tmp[idx,'est']> rfcv$InterpolationCutoff[idx]
-    OrangeLVI1[i] <- tmp[idx,'lower1sd']> rfcv$InterpolationCutoff[idx]
-
-    # Orange/Red for one c2_c1
-    idx <- which(tmp$var=='c2_c1')
-    RedVI2[i] <- tmp[idx,'est']> rfcv$NoiseCutoff
-    RedLVI2[i] <- tmp[idx,'lower1sd']> rfcv$NoiseCutoff
-    OrangeVI2[i] <- tmp[idx,'est']> rfcv$InterpolationCutoff[idx]
-    OrangeLVI2[i] <- tmp[idx,'lower1sd']> rfcv$InterpolationCutoff[idx]
+    ## Orange/Red Information
 
     # Above both lines
     tmp$IndCurve <- (tmp$est > rfcv$InterpolationCutoff)
@@ -78,6 +72,15 @@ for(c in 1:length(changes)){
     tmp[[paste0('iter',i)]] <- tmp$IndCurve
     VarVI_o <- merge(VarVI_o,tmp[,c('var',paste0('iter',i))])
 
+    # max Orange
+    tmp$IndCurve <- (tmp$est > max(rfcv$InterpolationCutoff))
+    tmp[[paste0('iter',i)]] <- tmp$IndCurve * tmp$IndNoise
+    VarVI_max <- merge(VarVI_max,tmp[,c('var',paste0('iter',i))])
+    tmp[[paste0('iter',i)]] <- tmp$IndCurve
+    VarVI_o_max <- merge(VarVI_o_max,tmp[,c('var',paste0('iter',i))])
+
+
+
     # 1 SD above both lines
     tmp$SDIndCurve <- (tmp$lower1sd > rfcv$InterpolationCutoff)
     tmp$SDIndNoise <- (tmp$lower1sd > rfcv$NoiseCutoff)
@@ -87,93 +90,95 @@ for(c in 1:length(changes)){
     VarLVI_r <- merge(VarLVI_r,tmp[,c('var',paste0('iter',i))])
     tmp[[paste0('iter',i)]] <- tmp$SDIndCurve
     VarLVI_o <- merge(VarLVI_o,tmp[,c('var',paste0('iter',i))])
+
+    # max Orange
+    tmp$IndCurve <- (tmp$lower1sd > max(rfcv$InterpolationCutoff))
+    tmp[[paste0('iter',i)]] <- tmp$IndCurve * tmp$IndNoise
+    VarLVI_max <- merge(VarLVI_max,tmp[,c('var',paste0('iter',i))])
+    tmp[[paste0('iter',i)]] <- tmp$IndCurve
+    VarLVI_o_max <- merge(VarLVI_o_max,tmp[,c('var',paste0('iter',i))])
   }
 
   results[[paste0('Change_',changes[c])]] <-
     list('VarVI'=VarVI, 'VarLVI'=VarLVI,
          'VarVI_r'=VarVI_r, 'VarLVI_r'=VarLVI_r,
          'VarVI_o'=VarVI_o, 'VarLVI_o'=VarLVI_o,
-         'RedVI1'=RedVI1, 'RedLVI1'=RedLVI1,
-         'OrangeVI1'=OrangeVI1, 'OrangeLVI1'=OrangeLVI1,
-         'RedVI2'=RedVI2, 'RedLVI2'=RedLVI2,
-         'OrangeVI2'=OrangeVI2, 'OrangeLVI2'=OrangeLVI2)
+         'VarVI_max'=VarVI_max, 'VarVI_o_max'=VarVI_o_max,
+         'VarLVI_max'=VarLVI_max, 'VarLVI_o_max'=VarLVI_o_max)
 }
 
 ## Save RDS
-#saveRDS(results,
-#results <- readRDS(
-#  paste0('C:/Users/j53vande/Downloads/',
-#         'change_sim.rds'))
+saveRDS(results, paste0('C:/Users/j53vande/Downloads/change_sim_small.rds'))
 
-# Look at c1_c2 / c2_c1
-# No -> Mild -> Extreme
+####################################
+# Look at c1_c2
+data_summary <- data.frame('var'=changes,
+                           'vert'=NA, 'lvert'=NA,
+                           'curve'=NA, 'lcurve'=NA,
+                           'mcurve'=NA, 'lmcurve'=NA,
+                           'both'=NA, 'lboth'=NA,
+                           'mboth'=NA, 'lmboth'=NA)
+
 for(i in 1:length(results)){
-  ## See how red/orange does for c1_c2
   # (vert)
-  sum(results[[i]]$RedVI1) / length(results[[i]]$RedVI1)
-  sum(results[[i]]$RedLVI1) / length(results[[i]]$RedLVI1)
-
-  sum(results[[i]]$VarVI_r[results[[i]]$VarVI_r$var=='c1_c2',-1]) /
+  data_summary[i,'vert'] <-
+    sum(results[[i]]$VarVI_r[results[[i]]$VarVI_r$var=='c1_c2',-1]) /
     length(results[[i]]$VarVI_r[results[[i]]$VarVI_r$var=='c1_c2',-1])
-  sum(results[[i]]$VarLVI_r[results[[i]]$VarLVI_r$var=='c1_c2',-1]) /
+  data_summary[i,'lvert'] <-
+    sum(results[[i]]$VarLVI_r[results[[i]]$VarLVI_r$var=='c1_c2',-1]) /
     length(results[[i]]$VarLVI_r[results[[i]]$VarLVI_r$var=='c1_c2',-1])
 
   # (curve)
-  sum(results[[i]]$OrangeVI1) / length(results[[i]]$OrangeVI1)
-  sum(results[[i]]$OrangeLVI1) / length(results[[i]]$OrangeLVI1)
-
-  sum(results[[i]]$VarVI_o[results[[i]]$VarVI_o$var=='c1_c2',-1]) /
+  data_summary[i,'curve'] <-
+    sum(results[[i]]$VarVI_o[results[[i]]$VarVI_o$var=='c1_c2',-1]) /
     length(results[[i]]$VarVI_o[results[[i]]$VarVI_o$var=='c1_c2',-1])
-  sum(results[[i]]$VarLVI_o[results[[i]]$VarLVI_o$var=='c1_c2',-1]) /
+  data_summary[i,'lcurve'] <-
+    sum(results[[i]]$VarLVI_o[results[[i]]$VarLVI_o$var=='c1_c2',-1]) /
     length(results[[i]]$VarLVI_o[results[[i]]$VarLVI_o$var=='c1_c2',-1])
 
+  # (max curve)
+  data_summary[i,'mcurve'] <-
+    sum(results[[i]]$VarVI_o_max[results[[i]]$VarVI_o_max$var=='c1_c2',-1]) /
+    length(results[[i]]$VarVI_o_max[results[[i]]$VarVI_o_max$var=='c1_c2',-1])
+  data_summary[i,'lmcurve'] <-
+    sum(results[[i]]$VarLVI_o_max[results[[i]]$VarLVI_o_max$var=='c1_c2',-1]) /
+    length(results[[i]]$VarLVI_o_max[results[[i]]$VarLVI_o_max$var=='c1_c2',-1])
+
   # (either)
-  sum(results[[i]]$VarVI[results[[i]]$VarVI$var=='c1_c2',-1]) /
+  data_summary[i,'both'] <-
+    sum(results[[i]]$VarVI[results[[i]]$VarVI$var=='c1_c2',-1]) /
     length(results[[i]]$VarVI[results[[i]]$VarVI$var=='c1_c2',-1])
-  sum(results[[i]]$VarLVI[results[[i]]$VarLVI$var=='c1_c2',-1]) /
+  data_summary[i,'lboth'] <-
+    sum(results[[i]]$VarLVI[results[[i]]$VarLVI$var=='c1_c2',-1]) /
     length(results[[i]]$VarLVI[results[[i]]$VarLVI$var=='c1_c2',-1])
 
-  # See how red/orange does for c2_c1
-  # (vert)
-  sum(results[[i]]$RedVI2) / length(results[[i]]$RedVI2)
-  sum(results[[i]]$RedLVI2) / length(results[[i]]$RedLVI2)
+  # (max either)
+  data_summary[i,'mboth'] <-
+    sum(results[[i]]$VarVI_max[results[[i]]$VarVI_max$var=='c1_c2',-1]) /
+    length(results[[i]]$VarVI_max[results[[i]]$VarVI_max$var=='c1_c2',-1])
+  data_summary[i,'lmboth'] <-
+    sum(results[[i]]$VarLVI_max[results[[i]]$VarLVI_max$var=='c1_c2',-1]) /
+    length(results[[i]]$VarLVI_max[results[[i]]$VarLVI_max$var=='c1_c2',-1])
 
-  sum(results[[i]]$VarVI_r[results[[i]]$VarVI_r$var=='c2_c1',-1]) /
-    length(results[[i]]$VarVI_r[results[[i]]$VarVI_r$var=='c2_c1',-1])
-  sum(results[[i]]$VarLVI_r[results[[i]]$VarLVI_r$var=='c2_c1',-1]) /
-    length(results[[i]]$VarLVI_r[results[[i]]$VarLVI_r$var=='c2_c1',-1])
 
-  # (curve)
-  sum(results[[i]]$OrangeVI2) / length(results[[i]]$OrangeVI2)
-  sum(results[[i]]$OrangeLVI2) / length(results[[i]]$OrangeLVI2)
-
-  sum(results[[i]]$VarVI_o[results[[i]]$VarVI_o$var=='c2_c1',-1]) /
-    length(results[[i]]$VarVI_o[results[[i]]$VarVI_o$var=='c2_c1',-1])
-  sum(results[[i]]$VarLVI_o[results[[i]]$VarLVI_o$var=='c2_c1',-1]) /
-    length(results[[i]]$VarLVI_o[results[[i]]$VarLVI_o$var=='c2_c1',-1])
-
-  # Either
-  sum(results[[i]]$VarVI[results[[i]]$VarVI$var=='c2_c1',-1]) /
-    length(results[[i]]$VarVI[results[[i]]$VarVI$var=='c2_c1',-1])
-  sum(results[[i]]$VarLVI[results[[i]]$VarLVI$var=='c2_c1',-1]) /
-    length(results[[i]]$VarLVI[results[[i]]$VarLVI$var=='c2_c1',-1])
-
-  # See how red/orange does in general
-  # (vert)
-  sum(results[[i]]$VarVI_r[-1]) /
-    (ncol(results[[i]]$VarVI_r[-1])*nrow(results[[i]]$VarVI_r[-1]))
-  sum(results[[i]]$VarLVI_r[-1]) /
-    (ncol(results[[i]]$VarLVI_r[-1])*nrow(results[[i]]$VarLVI_r[-1]))
-
-  # (curve)
-  sum(results[[i]]$VarVI_o[-1]) /
-    (ncol(results[[i]]$VarVI_o[-1])*nrow(results[[i]]$VarVI_o[-1]))
-  sum(results[[i]]$VarLVI_o[-1]) /
-    (ncol(results[[i]]$VarLVI_o[-1])*nrow(results[[i]]$VarLVI_o[-1]))
-
-  # (either)
-  sum(results[[i]]$VarVI[-1]) /
-    (ncol(results[[i]]$VarVI[-1])*nrow(results[[i]]$VarVI[-1]))
-  sum(results[[i]]$VarLVI[-1]) /
-    (ncol(results[[i]]$VarLVI[-1])*nrow(results[[i]]$VarLVI[-1]))
+  # # See how red/orange does in general
+  # # (vert)
+  # sum(results[[i]]$VarVI_r[-1]) /
+  #   (ncol(results[[i]]$VarVI_r[-1])*nrow(results[[i]]$VarVI_r[-1]))
+  # sum(results[[i]]$VarLVI_r[-1]) /
+  #   (ncol(results[[i]]$VarLVI_r[-1])*nrow(results[[i]]$VarLVI_r[-1]))
+  #
+  # # (curve)
+  # sum(results[[i]]$VarVI_o[-1]) /
+  #   (ncol(results[[i]]$VarVI_o[-1])*nrow(results[[i]]$VarVI_o[-1]))
+  # sum(results[[i]]$VarLVI_o[-1]) /
+  #   (ncol(results[[i]]$VarLVI_o[-1])*nrow(results[[i]]$VarLVI_o[-1]))
+  #
+  # # (either)
+  # sum(results[[i]]$VarVI[-1]) /
+  #   (ncol(results[[i]]$VarVI[-1])*nrow(results[[i]]$VarVI[-1]))
+  # sum(results[[i]]$VarLVI[-1]) /
+  #   (ncol(results[[i]]$VarLVI[-1])*nrow(results[[i]]$VarLVI[-1]))
 }
+
+data_summary
