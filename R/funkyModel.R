870dc365ee0d58aadacde1d926d7d33299665381
Jeremy <jeremyvanderdoes@gmail.com>
2023-07-09 22:15:52 -0400

    Simplify example for time considerations

--- R/funkyModel.R
+++ R/funkyModel.R
@@ -60,7 +60,7 @@
 #'   data = TNBC[, c(1:8, ncol(TNBC))],
 #'   outcome = "Class", unit = "Person",
 #'   metaNames = c("Age"),
-#'   nTrees = 50, synthetics = 10,
+#'   nTrees = 15, synthetics = 10,
 #'   silent = TRUE
 #' )
 #'
